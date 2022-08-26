2379168c6f3501a3f89b314707463e8920ddc789
Darya <daryalas17@gmail.com>
2022-08-26 18:58:47 +0300

    sem92

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+
+int GetNat(int a1, int b1)
+{ int S = a1;
+    
+            if (a1 == b1)
+            {
+                return S;
+            }
+            S = S + GetNat(a1 + 1, b1);
+            return S;
+       
+
+}
+Console.WriteLine(GetNat(n, m));
